1de171e206ca2691f5108c17c43759285899fb5c
Qingyun Xiu <qingyunx@msn.com>
2017-11-13 18:16:40 -0500

    Add files via upload

---
+++ src/app/material.module.ts
@@ -0,0 +1,27 @@
+﻿import { NgModule } from '@angular/core';
+
+import {
+  MatButtonModule,
+  MatMenuModule,
+  MatToolbarModule,
+  MatIconModule,
+  MatCardModule
+} from '@angular/material';
+
+@NgModule({
+  imports: [
+    MatButtonModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule
+  ]
+})
+export class MaterialModule {}
